ce766225b1572e66b16d57a1c2cc726a496fb892
ayeshN <ayesh@ascentic.se>
2020-05-10 16:56:06 +0530

    Added GetUserByEmail() method

--- B2CUserManagement/Interfaces/IUserManager.cs
+++ B2CUserManagement/Interfaces/IUserManager.cs
@@ -1,4 +1,5 @@
 ﻿using B2CUserManagement.Models;
+using Microsoft.Graph;
 using System.Threading.Tasks;
 
 namespace B2CUserManagement.Interfaces
@@ -6,6 +7,8 @@ namespace B2CUserManagement.Interfaces
     public interface IUserManager
     {
         Task CreateUser(B2CUser user);
+
+        Task<IGraphServiceUsersCollectionPage> GetUserByEmail(string email);
 
         void DeleteUser(string email);
     }
